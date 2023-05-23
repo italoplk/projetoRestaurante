c8bb1baf299c6d163d61af592b1d261cee7fefba
italoplk <idmachado@inf.ufpel.edu.br>
2023-05-23 00:42:22 -0300

    add transition menu

--- resources/script.js
+++ resources/script.js
@@ -2,6 +2,15 @@
 document.getElementById("hamburger").addEventListener("click", function(){
 
     document.querySelector(".main-nav").classList.toggle("active");
+    document.querySelector(".main-navs").classList.toggle("active");
 });
 
+
+document.getElementById("amburguer").addEventListener("click", function(){
+
+
+    document.querySelector(".main-nav2").classList.toggle("ativado");
+
+    document.querySelector("#amburguer").classList.toggle("rota");
+});
 
