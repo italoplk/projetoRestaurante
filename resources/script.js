0a88206cf9cca5949dd3a83b359c74151a32bf89
italoplk <idmachado@inf.ufpel.edu.br>
2023-05-30 02:13:03 -0300

    fix: menu burguer

--- resources/script.js
+++ resources/script.js
@@ -1,16 +1,11 @@
 
-document.getElementById("hamburger").addEventListener("click", function(){
+document.getElementById("hamburguer").addEventListener("click", function(){
 
-    document.querySelector(".main-nav").classList.toggle("active");
-    document.querySelector(".main-navs").classList.toggle("active");
+    document.querySelector(".main-nav").classList.toggle("ativado");
+    document.querySelector("#hamburguer").classList.toggle("rota");
+
+    document.querySelector(".ion-navicon-round").classList.toggle("mudacor");
+
 });
 
-
-document.getElementById("amburguer").addEventListener("click", function(){
-
-
-    document.querySelector(".main-nav2").classList.toggle("ativado");
-
-    document.querySelector("#amburguer").classList.toggle("rota");
-});
 
